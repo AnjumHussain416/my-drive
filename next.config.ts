998d2638b42f1632f3d95d860e6db06e79217cd9
AnjumBinu <anjum@workspaceit.com>
2025-12-07 14:13:04 +0600

    my drive structuring

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: "export",
   // basePath: "/next-export/out",
   // assetPrefix: "/next-export/out/",
+  trailingSlash: true,
 };
 
 export default nextConfig;
